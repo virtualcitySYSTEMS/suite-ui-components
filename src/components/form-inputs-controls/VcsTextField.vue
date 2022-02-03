8ca10face9f35c73edc771a9e963fc021afe21a2
Roland Ruhdorfer <rruhdorfer@vc.systems>
2022-02-03 07:48:49 +0000

    VcsTextField

--- src/components/form-inputs-controls/VcsTextField.vue
+++ src/components/form-inputs-controls/VcsTextField.vue
@@ -3,30 +3,10 @@
     class="d-flex flex-row align-end justify-space-between"
     :class="[...customClasses]"
   >
-    <slot name="label">
-      <label
-        v-if="!useNativeLabel"
-        class="mr-3"
-        :class="{ caption: $attrs.dense !== undefined }"
-      >
-        {{ label }}
-      </label>
-    </slot>
-
-
     <slot name="input">
       <div class="d-flex justify-center align-center w-full">
         <v-text-field
-          :class="{
-            caption: $attrs.dense !== undefined,
-            'text-right': alignInputContent === 'right',
-            'text-left': alignInputContent === 'left',
-            'text-center': alignInputContent === 'center',
-            'input--seconds': unit === 'sec',
-            'input--meters': unit === 'm',
-            'input--degrees': unit === 'deg',
-          }"
-          v-bind="{...$props, ...$attrs, label: useNativeLabel ? $props.label : undefined}"
+          v-bind="{...$props, ...$attrs}"
           @input="$emit('input', $event)"
         />
       </div>
@@ -51,52 +31,6 @@
 
   label {
     white-space: nowrap;
-  }
-
-  .input--seconds,
-  .input--meters,
-  .input--degrees {
-    position: relative;
-
-    &::after {
-      position: absolute;
-      right: 20px;
-      bottom: 6px;
-      color: black;
-    }
-
-    input.v-input {
-      padding-right: 42px;
-      text-align: right;
-    }
-  }
-
-  .v-input--dense {
-    &.input--meters,
-    &.input--seconds,
-    &.input--degrees {
-      &::after {
-        bottom: 2px;
-      }
-    }
-  }
-
-  .input--seconds {
-    &::after {
-      content: 'sec';
-    }
-  }
-
-  .input--meters {
-    &::after {
-      content: 'm';
-    }
-  }
-
-  .input--degrees {
-    &::after {
-      content: '°';
-    }
   }
 
   // Firefox only
@@ -128,34 +62,17 @@
   import Vue from 'vue';
 
   /**
-   * @description extends API of https://vuetifyjs.com/en/api/v-text-field/
-   * @vue-prop {string}   label                                       - Label to be shown before the input
-   * @vue-prop {('left' | 'center' | 'right')}   alignInputContent    - Direction to which the content of the input field should be oriented
-   * @vue-prop {boolean}   useNativeLabel                             - When this is set to true the component will use vuetify floating labels
-   * @vue-prop {string[]}   customClasses                             - Array of string CSS classes which will be applied to the container
+   * @description extends API of {@link https://vuetifyjs.com/en/api/v-text-field/|vuetify v-text-field}
+   * @vue-prop {string[]}   [customClasses=[]]  - Array of string CSS classes which will be applied to the container
+   * @vue-data {slot} [#input] - slot to override input
+   * @vue-event {InputEvent} input - text emitted on input
    */
   export default Vue.extend({
     name: 'VcsTextField',
     props: {
-      label: {
-        type: String,
-        default: undefined,
-      },
-      alignInputContent: {
-        type: String,
-        default: 'left',
-      },
-      useNativeLabel: {
-        type: Boolean,
-        default: false,
-      },
       customClasses: {
         type: Array,
         default: () => ([]),
-      },
-      unit: {
-        type: String,
-        default: undefined, // 'sec' | 'm' | 'deg'
       },
     },
   });
