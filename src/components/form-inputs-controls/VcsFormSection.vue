711848d196f020f07203e0766685f62beca28b49
Roland Ruhdorfer <rruhdorfer@vc.systems>
2022-02-03 12:24:36 +0000

    VcsFormSection

--- src/components/form-inputs-controls/VcsFormSection.vue
+++ src/components/form-inputs-controls/VcsFormSection.vue
@@ -5,13 +5,14 @@
         <div class="form-section-header d-flex justify-space-between align-center">
           <strong class="caption">{{ title }}</strong>
 
-          <div v-if="titleActions" class="form-section-header__actions">
+          <div v-if="titleActions.length" class="form-section-header__actions">
             <v-icon
               v-for="(action, index) of titleActions"
               :key="action.name"
               v-text="action.icon"
               :size="action.size || '16'"
               :class="{ 'mr-1': titleActions.length > 1 && (index + 1) < titleActions.length }"
+              @click="$emit('click', action)"
             />
           </div>
         </div>
@@ -28,10 +29,22 @@
   import Vue from 'vue';
 
   /**
+   * // TODO standardize Action Items https://gitlab.virtualcitysystems.de/vcsuite/virtualcitymap_ui/-/issues/64
+   * @typedef {Object} module:VcsFormSection.TitleAction
+   * @property {string} name - name of the action
+   * @property {string} icon - icon to display
+   * @property {number|string} [size=16] - size of the icon
+   * @module VcsFormSection
+   */
+
+  /**
    * @description
    * Stylized form section with action buttons
+   * @vue-data {slot} [#title] - slot to override form section header
+   * @vue-data {slot} [#default] - slot with the section content
    * @vue-prop {string}   title - Title to be displayed
-   * @vue-prop {Array}    titleActions - Icons to be displayed on the right side
+   * @vue-prop {Array<TitleAction>}    titleActions - Icons to be displayed on the right side
+   * @vue-event {TitleAction} click   - icons emit corresponding TitleAction on click
    */
   export default Vue.extend({
     name: 'VcsFormSection',
@@ -42,7 +55,7 @@
       },
       titleActions: {
         type: Array,
-        default: () => ([]), // { name, icon, size }
+        default: () => ([]),
       },
     },
   });
