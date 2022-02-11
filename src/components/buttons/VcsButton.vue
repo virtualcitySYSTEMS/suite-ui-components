6b13554ed5b34b671b4c5ae6a3163640eae4c9e7
Tobias Krauth <tkrauth@vc.systems>
2022-02-11 07:12:52 +0000

    Forward vuetify tooltip props

--- src/components/buttons/VcsButton.vue
+++ src/components/buttons/VcsButton.vue
@@ -10,15 +10,15 @@
       :top="tooltipPosition === 'top'"
       :left="tooltipPosition === 'left'"
       :right="tooltipPosition === 'right'"
+      v-bind="{...mergedTooltipProps}"
     >
       <template #activator="{ on, attrs }">
         <v-btn
           :color="backgroundColor"
           :text="isTextButton"
-          @click="$emit('click', $event)"
           class="ma-1 px-2"
           v-bind="{...$attrs, attrs}"
-          v-on="on"
+          v-on="{...$listeners, ...on}"
         >
           <v-icon v-if="icon" v-text="icon" :class="{ 'mr-2': hasDefaultSlot }" />
           <slot />
@@ -30,15 +30,15 @@
       v-else
       :color="backgroundColor"
       :text="isTextButton"
-      @click="$emit('click', $event)"
       class="ma-1 px-2"
       v-bind="{...$attrs}"
+      v-on="$listeners"
     >
       <v-icon v-if="icon" v-text="icon" :class="{ 'mr-2': hasDefaultSlot }" />
       <slot />
     </v-btn>
 
-    <Badge
+    <VcsBadge
       v-if="hasUpdate"
       :color="'warning'"
       class="position-absolute pos-t-0 pos-r-0"
@@ -61,13 +61,14 @@
 <script>
   import Vue from 'vue';
 
-  import Badge from '../notification/VcsBadge.vue';
+  import VcsBadge from '../notification/VcsBadge.vue';
 
   /**
    * @description a button with tooltip extending {@link https://vuetifyjs.com/en/api/v-btn/|vuetify v-btn} using
    * {@link https://vuetifyjs.com/en/api/v-tooltip/|vuetify v-tooltip}.
    * @vue-prop {boolean}                                isActive - Whether button has background color. Applies vuetify primary color if color property is not set.
-   * @vue-prop {string}                                 tooltip - Text content of a tooltip which appears on hover.
+   * @vue-prop {string}                                 tooltip - Text content of a tooltip which appears on hover with default delay.
+   * @vue-prop {Object}                                 [tooltipProps={openDelay: 200}] - Properties to be passed to vuetify tooltip API. 'bottom', 'top', 'left', 'right' not supported. Use tooltipPosition instead.
    * @vue-prop {('bottom' | 'left' | 'top' | 'right')}  tooltipPosition - Position of the tooltip.
    * @vue-prop {boolean}                                hasUpdate - Whether the button shows a badge in the top right.
    * @vue-prop {string}                                 icon - When given, will display an icon in the button. Replaces vuetify icon property.
@@ -77,7 +78,8 @@
    */
   export default Vue.extend({
     name: 'VcsButton',
-    components: { Badge },
+    components: { VcsBadge },
+    inheritAttrs: false,
     computed: {
       arrowClass() {
         switch (this.tooltipPosition) {
@@ -101,6 +103,9 @@
       isTextButton() {
         return !this.isActive ? true : null;
       },
+      mergedTooltipProps() {
+        return { ...{ openDelay: 200 }, ...this.tooltipProps };
+      },
     },
     props: {
       isActive: {
@@ -114,6 +119,10 @@
       tooltipPosition: {
         type: String,
         default: 'bottom',
+      },
+      tooltipProps: {
+        type: Object,
+        default: () => ({}),
       },
       hasUpdate: {
         type: Boolean,
